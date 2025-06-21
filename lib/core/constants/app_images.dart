9935ccdbde3dde4a031caf5c11c573d1fac26d29
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-21 23:14:10 +0300

    adding app images

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,5 @@
+abstract class AppImages {
+  static const String imageNotAvailable =
+      "assets/images/Image_not_available.png";
+  static const String emptyAnimation = "assets/animations/empty_animation.json";
+}
