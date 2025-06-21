10547a98a37fdf97911981c9b0b13964e208e48e
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-21 23:13:54 +0300

    adding app colors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  static const Color blue = Color(0xff0E3487);
+  static const Color darkBlue = Color(0xff004087);
+  static const Color lightBlue = Color(0xff5089B5);
+  static const Color indigo = Color(0xff3A3760);
+  static const Color black = Color(0xff000000);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color offWhite = Color(0xffFEFFE8);
+  static const Color gray = Color(0xff9E9E9E);
+  static const Color semiGray = Color(0xffB8CCE0);
+  static const Color red = Color(0xffFE4039);
+  static const Color yellow = Color(0xffFFD800);
+}
