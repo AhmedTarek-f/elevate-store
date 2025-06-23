6816aba7e83cd7fbffd72c4c2ddb3847a12739a4
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-23 15:27:49 +0300

    adding configure dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:elevate_store/core/di/di.dart';
 import 'package:elevate_store/elevate_store_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  configureDependencies();
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
