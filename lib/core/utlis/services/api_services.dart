46f67fae522e86b985ce6c902a2d7504a05bbb4f
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-23 15:27:16 +0300

    updaing api service class to be singleton

--- lib/core/utlis/services/api_services.dart
+++ lib/core/utlis/services/api_services.dart
@@ -2,9 +2,11 @@ import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:elevate_store/core/utlis/exceptions/dio_exceptions.dart';
 import 'package:elevate_store/core/utlis/exceptions/failure.dart';
+import 'package:injectable/injectable.dart';
 
-abstract class ApiService {
-  static final Dio _dio = Dio(
+@singleton
+class ApiService {
+  final _dio = Dio(
     BaseOptions(
       baseUrl: "https://fakestoreapi.com",
       headers: {"Content-Type": "application/json"},
@@ -12,7 +14,7 @@ abstract class ApiService {
   );
 
   // GET Request
-  static Future<Either<Failure, Response>> getRequest({
+  Future<Either<Failure, Response>> getRequest({
     required String endpoint,
     Map<String, dynamic>? queryParams,
     Map<String, String>? headers,
