e5a04f4daf0f1a25449f2d224d919178a4815b88
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-23 15:25:31 +0300

    using git it

--- lib/core/utlis/router/app_router.dart
+++ lib/core/utlis/router/app_router.dart
@@ -1,4 +1,6 @@
+import 'package:elevate_store/core/di/di.dart';
 import 'package:elevate_store/core/utlis/router/route_names.dart';
+import 'package:elevate_store/features/home/domain/repositories/home_repository.dart';
 import 'package:elevate_store/features/home/presentation/views/home_view.dart';
 import 'package:elevate_store/features/home/presentation/views_model/home_cubit.dart';
 import 'package:flutter/material.dart';
@@ -7,7 +9,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 abstract class AppRouter {
   static Map<String, Widget Function(BuildContext)> routes = {
     RouteNames.home: (_) => BlocProvider<HomeCubit>(
-      create: (context) => HomeCubit(),
+      create: (context) => HomeCubit(homeRepo: getIt<HomeRepository>()),
       child: const HomeView(),
     ),
   };
