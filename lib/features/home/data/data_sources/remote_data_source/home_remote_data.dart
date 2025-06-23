da0fd8b9155b96f18353bc33b1f91528566453f1
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-23 15:33:28 +0300

    adding home remote data

---
+++ lib/features/home/data/data_sources/remote_data_source/home_remote_data.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:elevate_store/core/utlis/exceptions/failure.dart';
+import 'package:elevate_store/features/home/data/models/product_model/product_model.dart';
+
+abstract class HomeRemoteData {
+  Future<Either<Failure, List<ProductModel>>> fetchProducts();
+}
